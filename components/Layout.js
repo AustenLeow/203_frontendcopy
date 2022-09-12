84a4f446a6e0b65a949fd04a9605c33084d65344
Caryn Chan <carynchan21@gmail.com>
2022-09-12 18:33:06 +0800

    layout basics and homepage

---
+++ components/Layout.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Head from 'next/head'
+import Navbar from './Navbar'
+
+export default function Layout({ title, children}) {
+    return (
+        <>
+          <Head>
+            <title>{title ? title + ' - re_' : 're_'}</title>
+            <meta name="description" content="climate change" />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+
+          <div className="flex min-h-screen flex-col justify-between bg-[#EFEDE7]">
+            <header>
+                
+            </header>
+            <main className="container h-screen m-auto mt-4 p-4 bg-[#EFEDE7]">{children} 
+                
+            </main>
+            <footer className="flex h-[400px] justify-center items-center shadow-inner bg-[#EFEDE7]">footer</footer>
+          </div>
+          
+          
+        </>
+      )
+}
