293a7862e48aeda1a17a69c0baf3363e58935501
Caryn Chan <carynchan21@gmail.com>
2022-09-15 20:28:19 +0800

    login validation

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,7 @@
-import React from "react";
-import Layout from "../components/Layout";
 import Link from "next/link";
+import React from "react";
 import { useForm } from "react-hook-form";
+import Layout from "../components/Layout";
 
 export default function login() {
   const {
@@ -9,58 +9,57 @@ export default function login() {
     register,
     formState: { errors },
   } = useForm();
-
-  const submitHandler = ({ username, password }) => {
-    console.log(username, password);
+  const submitHandler = ({ email, password }) => {
+    console.log(email, password);
   };
-
   return (
-    <Layout title="login">
+    <Layout title="Login">
       <form
         className="mx-auto max-w-screen-md"
         onSubmit={handleSubmit(submitHandler)}
       >
-        <div className="p-10 bg-[#F5F5F5]">
-          <h1 className="mb-7 text-xl">Login</h1>
-          <div className="w-mb-9">
-            <label htmlFor="username">Email</label>
-            <input
-              type="text"
-              {...register("username", {
-                required: "Please enter username",
-                pattern: {
-                  value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
-                  message:
-                    "Please enter valid username with 1 capital letter, 1 lower letter",
-                },
-              })}
-              id="username"
-              name="username"
-              className="w-full mb-5"
-              autoFocus
-            ></input>
-            {errors.username && (
-              <div className="text-red-500">{errors.username.message}</div>
-            )}
-          </div>
-          <div className="mb-5">
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              className="w-full"
-              id="password"
-              autoFocus
-            ></input>
-          </div>
-          <div className="mb-4">
-            <button className="bg-blue-500 text-white w-full rounded py-2">
-              Login
-            </button>
-          </div>
-          <div className="mb-4">
-            Don&apos;t have an account? &nbsp;
-            <Link href="/signup">Sign up </Link>
-          </div>
+        <h1 className="mb-4 text-xl">Login</h1>
+        <div className="mb-4">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            {...register("email", {
+              required: "Please enter email",
+              pattern: {
+                value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
+                message: "Please enter valid email",
+              },
+            })}
+            className="w-full"
+            id="email"
+            autoFocus
+          ></input>
+          {errors.email && (
+            <div className="text-red-500">{errors.email.message}</div>
+          )}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            {...register("password", {
+              required: "Please enter password",
+              minLength: { value: 6, message: "password is more than 5 chars" },
+            })}
+            className="w-full"
+            id="password"
+            autoFocus
+          ></input>
+          {errors.password && (
+            <div className="text-red-500 ">{errors.password.message}</div>
+          )}
+        </div>
+        <div className="mb-4 ">
+          <button className="primary-button">Login</button>
+        </div>
+        <div className="mb-4 ">
+          Don&apos;t have an account? &nbsp;
+          <Link href="register">Sign up</Link>
         </div>
       </form>
     </Layout>
