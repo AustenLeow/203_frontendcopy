97fee186981952b083b1c58106eb698c99fbff1b
Caryn Chan <carynchan21@gmail.com>
2022-09-20 18:14:16 +0800

    added dependencies and titles

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Layout from "../components/Layout";
+import Typical from "react-typical";
 
 export default function Home() {
   return (
@@ -12,7 +13,30 @@ export default function Home() {
           loop
           muted
         />
-        
+        <div className="absolute w-4/5 h-fit m-auto grid grid-cols-1 lg:grid-cols-2">
+          <div className="text-6xl text-white">
+            <strong> re</strong>
+            <strong className="px-2">
+              <Typical
+                steps={[
+                  "price_",
+                  1000,
+                  "planet_",
+                  1000,
+                  "food_",
+                  1000,
+                  "use_",
+                  1000,
+                  "duce_",
+                  1000,
+                ]}
+                wrapper="p"
+                loop={Infinity}
+              />
+            </strong>
+          </div>
+          <div className="text-end items-end justify-center"> taste, don't taste </div>
+        </div>
       </div>
     </Layout>
   );
