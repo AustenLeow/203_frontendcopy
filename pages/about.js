4d3830c0388af86380e3dc3fefb944888358f2aa
Caryn Chan <carynchan21@gmail.com>
2022-09-23 19:24:25 +0800

    about

--- pages/about.js
+++ pages/about.js
@@ -33,12 +33,31 @@ export default function about() {
           </div>
         </div>
         <div className="h-screen w-4/6 m-auto grid grid-cols-1 lg:grid-cols-2">
-          <div className="flex flex-col items-start justify-center text-4xl font-bold">
-            hello
-          </div>
-          <div className="flex flex-col items-start justify-center text-4xl font-bold">
+        <div className="font-light flex flex-col items-start justify-center ">
+        <div className="pb-10 flex flex-col items-end justify-center text-4xl font-bold text-[#4E632E]">
             our story
+            </div>
+            <div className="pb-4">
+              food wastage has been a major contributor to our climate.
+            </div>
+
+            <div className="pb-4">
+              re: targets to help the climate by reducing food wastage.{" "}
+            </div>
+            <div className="pb-4">
+              to achieve this goal, re: was invented to be the bridge between
+              consumers and businesses.{" "}
+            </div>
+            <div className="pb-4">
+              re: does this through its marketplace where customers can buy
+              near-expired, good condition foods.{" "}
+            </div>
           </div>
+          
+            <div className="flex items-center justify-center">
+              *add image 
+            </div>
+         
         </div>
   
     </Layout>
