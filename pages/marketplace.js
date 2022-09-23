559433264a6b65497b5fb4ac61d356c0b0750216
Caryn Chan <carynchan21@gmail.com>
2022-09-23 19:14:27 +0800

    about done

--- pages/marketplace.js
+++ pages/marketplace.js
@@ -3,6 +3,14 @@ import Layout from '../components/Layout'
 
 export default function marketplace() {
   return (
-    <div>marketplace<Layout></Layout></div>
+    <div>
+        <Layout>
+        <div className="h-screen w-4/6 m-auto grid grid-cols-1 lg:grid-cols-2">
+          <div className="flex flex-col items-start justify-center text-4xl font-bold">
+            
+            </div>
+          </div>
+        </Layout>
+    </div>
   )
 }
