cbfbdd61865d6e0eaa8aabd913348af29674d59c
Caryn Chan <carynchan21@gmail.com>
2022-09-23 18:04:03 +0800

    navbar sticky

--- components/Layout.js
+++ components/Layout.js
@@ -1,23 +1,23 @@
 import React from "react";
 import Head from "next/head";
 import Footer from "../components/Footer";
+import Navbar from "./Navbar";
 
 export default function Layout({ title, children }) {
   return (
     <>
       <Head>
-        <title>{title ?  " re:" + title : "re:"}</title>
+        <title>{title ? " re:" + title : "re:"}</title>
         <meta name="description" content="climate change" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+
       <div className="w-screen flex flex-col justify-center bg-[#EFEDEE]">
+        <Navbar />
         <main className="w-screen bg-[#EFEDEE] items-center justify-center">
           {children}{" "}
         </main>
-        {/* <footer className="flex h-[320px] justify-center items-center shadow-inner bg-[#EFEDE7]"> */}
-          <Footer/>
-        {/* </footer> */}
+        <Footer />
       </div>
     </>
   );
