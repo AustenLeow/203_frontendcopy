6becef3441e31378d335d2b3b0c0ac4f56b87bd9
risheetha28 <risheethas.2021@smu.edu.sg>
2022-09-26 18:31:09 +0800

    updated marketplace

--- pages/marketplace.js
+++ pages/marketplace.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Layout from '../components/Layout';
+import data from '../utils/data';
+import ProductItem from '../components/ProductItem';
 
 export default function marketplace() {
   return (
-    <div>marketplace</div>
-  )
+    <Layout title="Marketplace">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductItem
+            product={product}
+            key={product.slug}
+            // addToCartHandler={addToCartHandler}
+          ></ProductItem>
+        ))}
+      </div>
+    </Layout>
+  );
 }
