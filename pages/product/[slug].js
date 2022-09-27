02a164e1eada379b9c79bad8fc2f442ed1aa6e8b
Caryn Chan <carynchan21@gmail.com>
2022-09-27 20:00:43 +0800

    product page

---
+++ pages/product/[slug].js
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React from "react";
+import Layout from "../../components/Layout";
+import data from "../../utils/data";
+import { BiArrowBack } from "react-icons/bi";
+
+export default function ProductScreen() {
+  const { query } = useRouter();
+  const { slug } = query;
+  const product = data.products.find((x) => x.slug === slug); // getting product from slug
+
+  // if product does not exist
+  if (!product) {
+    return <div>Produt Not Found</div>;
+  }
+
+  return (
+    <Layout title={product.name}>
+      <div className="mx-auto py-16 md:py-32 w-3/4">
+        <div className="p-4">
+          {" "}
+          <BiArrowBack />{" "}
+        </div>
+        <div className="w-full h-fit m-auto grid grid-cols-1 lg:grid-cols-2 ">
+          <div className="p-20 hover:p-15">
+            <Image className="object-cover w-2/3"
+              src={product.image}
+              alt={product.name}
+              width={700}
+              height={640}
+              layout="responsive"
+            ></Image>
+          </div>
+          <div className="text-left flex flex-col justify-center shadow-2xl p-10">
+            <div className="h-2/5">title</div>
+            <div> desc</div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
