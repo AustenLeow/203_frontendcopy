1180c72fbdfab318d3f27de067b41a6165d27bba
Caryn Chan <carynchan21@gmail.com>
2022-10-03 17:59:58 +0800

    formik log in

--- pages/login.js
+++ pages/login.js
@@ -3,8 +3,22 @@ import { useForm } from "react-hook-form";
 import Layout from "../components/Layout";
 import axios from "axios";
 import { signIn } from "next-auth/react";
+import { useFormik } from "formik";
+import Link from "next/link";
 
 export default function login() {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit,
+  });
+
+  async function onSubmit(values) {
+    console.log(values);
+  }
+
   const {
     handleSubmit,
     register,
@@ -26,19 +40,18 @@ export default function login() {
       });
   };
 
-  async function handleGoogleSignIn(){
-    signIn('google', {callbackUrl: 'http://localhost:3000/'})
+  async function handleGoogleSignIn() {
+    signIn("google", { callbackUrl: "http://localhost:3000/" });
   }
 
-  async function handleGitHubSignIn(){
-    signIn('github', {callbackUrl: 'http://localhost:3000/'})
+  async function handleGitHubSignIn() {
+    signIn("github", { callbackUrl: "http://localhost:3000/" });
   }
 
   return (
     <Layout title="login">
-      <form
+      <div
         className="mx-auto w-4/5 py-16 md:py-32"
-        onSubmit={handleSubmit(submitHandler)}
       >
         <div className="w-full flex items-center justify-center">
           <div className="w-full h-fit m-auto grid grid-cols-1 lg:grid-cols-2 ">
@@ -46,11 +59,11 @@ export default function login() {
               image should be here
             </div>
             <div className="text-left flex flex-col justify-center shadow-2xl p-10">
-              <h1 className="mb-4 login-text">Login</h1>
+              {/* <h1 className="mb-4 login-text">Login</h1>
               <div className="mb-4">
                 <label htmlFor="username">Username</label>
                 <input
-                  type="username" placeholder="Email"
+                  type="username" placeholder="Email" onChange={formik.handleChange} value={formik.values.username}
                   {...register("username", {
                     required: "Please enter your username",
                     minLength: {
@@ -71,7 +84,7 @@ export default function login() {
               <div className="mb-4">
                 <label htmlFor="password">Password</label>
                 <input
-                  type="password" placeholder="Password"
+                  type="password" placeholder="Password" onChange={formik.handleChange} value={formik.values.password}
                   {...register("password", {
                     required: "Please enter your password",
                     minLength: {
@@ -116,11 +129,60 @@ export default function login() {
                 <button onClick={handleGitHubSignIn} className="primary-button hover:bg-[#4E632E]">
                   GitHub 
                 </button>
-              </div>
+              </div> */}
+              <div className="text-2xl py-4 font-bold">Login</div>
+              <form className="flex flex-col gap-5" onSubmit={formik.handleSubmit}>
+                <div className="w-full input">
+                  <input
+                    type="username"
+                    name="username"
+                    placeholder="Username"
+                    onChange={formik.handleChange}
+                    value={formik.values.username}
+                  />
+                </div>
+                <div className="w-full input">
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    onChange={formik.handleChange}
+                    value={formik.values.password}
+                  />
+                </div>
+
+                <div>
+                  <button
+                    type="submit"
+                    className="w-full primary-button hover:bg-[#4E632E]"
+                  >
+                    Login
+                  </button>
+                </div>
+                <div>
+                  <button className="w-full p-2 rounded border-2" onClick={handleGoogleSignIn} type="submit">
+                    {" "}
+                    Sign in with Google{" "}
+                  </button>
+                </div>
+                <div>
+                  <button className="w-full p-2 rounded border-2" onClick={handleGitHubSignIn} type="submit">
+                    {" "}
+                    Sign in with GitHub{" "}
+                  </button>
+                </div>
+              </form>
+              <p className="text-center text-grey-400">
+                {" "}
+                Don't have an account?
+                <Link href={"/signup"}>
+                  <a className="text-green>"></a>
+                </Link>
+              </p>
             </div>
           </div>
         </div>
-      </form>
+      </div>
     </Layout>
   );
 }
