b1c01f4a3015ad812b631e838375b72e7e95fb7e
risheetha28 <risheethas.2021@smu.edu.sg>
2022-10-10 20:16:50 +0800

    display items from backend to marketplace

--- pages/marketplace.js
+++ pages/marketplace.js
@@ -7,34 +7,68 @@ import { useState, useEffect, useCallback } from 'react';
 
 export default function marketplace() {
 
-  const router = useRouter();
+  const [items, setItems] = useState([]);
+
   useEffect(() => {
     fetchItemsHandler();
-}, []);
+  }, []);
 
+  const router = useRouter();
   function logout() {
     localStorage.removeItem("token")
     router.push("/login")
   }
 
   function fetchItemsHandler() {
-        
-    const items = JSON.parse((localStorage.getItem("items")|| "[]"));
+    const items = JSON.parse((localStorage.getItem("items") || "[]"));
     console.log(items);
+    setItems(items);
+  }
+
+  async function addToCart (item) {
+    fetch(`http://localhost:8080/api/v1/cart/add/${item.id}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        "Authorization": "Bearer " + localStorage.getItem("token")
+      },
+    }).then(response => response.text())
+    .then(item => {
+      console.log(item);
+      
+    })
+    .catch(err => {
+      console.log(err);
+    })
   }
 
   return (
     <Layout title="marketplace">
-      <div className="p-10">
-      <p><button onClick={logout}>Log out</button></p>
-      <div className="grid grid-cols-1 gap-8 md:grid-cols-3 lg:grid-cols-4">
-        {data.products.map((product) => (
-          <ProductItem
-            product={product}
-            key={product.slug}
-          ></ProductItem>
-        ))}
-      </div>
+      <div className="px-5">
+        <h1 className="py-3">Marketplace</h1>
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-3 lg:grid-cols-4">
+          {items.map(item =>
+            <div key={item.id}>
+              <p><img src={item.url} width={100} height={100}></img></p>
+              <div className="py-5">
+                <p className="font-bold text-xl">{item.itemName}</p>
+                <p className="font-light text-xs">{item.brand}</p>
+              </div>
+              <p>Price: ${item.price}</p>
+              <p>Quantity: {item.quantity}</p>
+              <p>Expires on: {item.expiry_date}</p>
+              <button
+                className="product-button w-full"
+                type="button"
+                onClick={() => addToCart(item)}
+              >
+                <div> Add to cart</div>
+              </button>
+            </div>
+          )}
+        </div>
+
+
       </div>
     </Layout>
   );
