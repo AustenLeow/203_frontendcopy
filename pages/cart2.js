5cdbfbbad4ebb2ecf26a5eadabcf5e086abfe0dc
Austen Leow <austenleow.al@gmail.com>
2022-10-10 19:31:24 +0800

    add removeItemHandler and css

--- pages/cart2.js
+++ pages/cart2.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect} from 'react';
+import Layout from "../components/Layout";
+// import Image from 'next/image';
+import { XCircleIcon } from '@heroicons/react/outline/esm';
 
 export default function cart2() {
+
+    const total = 0;
 
     const [cart, setCart] = useState([]);
 
@@ -13,34 +18,80 @@ export default function cart2() {
         console.log(cart);
         setCart(cart);
     }
+
+    const removeItemHandler = (product) => {
+        fetch(`http://localhost:8080/api/v1/cart/delete/${product.id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                "Authorization": "Bearer " + localStorage.getItem("token")
+            },
+            }).then(response => response.text())
+            .then(product => {
+            console.log(product);
+            fetchCartItemsHandler()
+            })
+            .catch(err => {
+            console.log(err);
+            })
+    };
     
     return (
-        <React.Fragment>
-              <div >
-                <h1>Cart</h1>
-                <table>
-                <tbody>
+        <Layout title="cart">
+                <h1 className='text-xl p-7'>Your Shopping Cart</h1>
+                { cart.length == 0 ? (<div className='px-7'>
+                    Cart is empty. <a className='text-[#687259]' href='/marketplace'>Go shopping</a>
+                </div>) : 
+                (
+                <div className='grid md:grid-cols-4 md:gap-5 px-7'>
+                <div className='overflow-x-auto md:col-span-3'>
+                <table className='min-w-full'>
+                <thead className='border-b'>
                 <tr>
-                    <th>item</th>
-                    <th> image </th>
-                    <th>price</th>
-                    <th>quantity</th>
-                    <th>subtotal</th>
+                    <th className='px-5 text-right'>item</th>
+                    <th className='px-5 text-right'></th>
+                    <th className='px-5 text-right'>price</th>
+                    <th className='px-5 text-right'>quantity</th>
+                    <th className='px-5 text-right'>subtotal</th>
+                    <th className='px-5'>Action</th>
                 </tr>
-                
-                  {cart.map(cartitem => 
-                    <tr key={cartitem.item.id}>
-                      <td>{cartitem.item.itemName}</td>
-                      <td><img src={cartitem.item.url} width={100} height={100}></img></td>
-                      <td>{cartitem.item.price}</td>
-                     <td>{cartitem.quantity}</td>
-                     <td>{cartitem.subtotal}</td>
+                </thead>
+                <tbody>
+                  {cart.map((cartitem) => 
+                    <tr key={cartitem.item.id} className='border-b'>
+                      <td className='p-5 text-right'>{cartitem.item.itemName}</td>
+                      <td className='p-5 text-right'><img src={cartitem.item.url} alt={cartitem.item.itemName} className='flex items-center' width={100} height={100}></img></td>
+                      <td className='p-5 text-right'>{cartitem.item.price}</td>
+                     <td className='p-5 text-right'>{cartitem.quantity}</td>
+                     <td className='p-5 text-right'>{cartitem.subtotal}</td>
+                     <td className='p-5 text-center'>
+                        <button onClick={() => removeItemHandler(cartitem.item)}>
+                            <XCircleIcon className='h-5 w-5'></XCircleIcon>
+                        </button>
+                    </td>
                     </tr>
                   )}
                 </tbody>
                   </table>
-              </div>
-        </React.Fragment>
+                  </div>
+                  <div className='card p-5'>
+                        <ul>
+                            {/* <li>
+                                <div className='pb-3 text-xl'>
+                                    Subtotal ${cartitem.subtotal}
+                                </div>
+                            </li> */}
+                            {/* check out */}
+                            <li>
+                                <button className='primary-button w-full'>Check Out</button>
+                            </li>
+                        </ul>
+                    </div>
+                  </div>
+                )}
+             
+            
+        </Layout>
         );
 }
 
