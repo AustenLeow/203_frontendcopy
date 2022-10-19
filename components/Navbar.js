e5138f6d0b8640a64ae21c96a5e7b5525bfe298b
Caryn Chan <carynchan21@gmail.com>
2022-10-19 11:13:05 +0800

    card design

--- components/Navbar.js
+++ components/Navbar.js
@@ -164,7 +164,6 @@ export default function Navbar() {
             <NavLink className="no-underline hover:underline" to="/about">
               about
             </NavLink>
-            <NavLink to="/marketplace">marketplace</NavLink>
             <NavLink to="/login">login</NavLink>
             <NavLink to="/signup">sign up</NavLink>
            {/* <NavLink to="/cart2">
