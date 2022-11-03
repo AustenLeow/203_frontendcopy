97e1479eef8a65ae6bcc733c4ca12636c14a3009
risheetha28 <risheethas.2021@smu.edu.sg>
2022-11-04 02:38:41 +0800

    order history page

--- components/UserOrderHistory.js
+++ components/UserOrderHistory.js
@@ -1,22 +1,12 @@
 import { getClientBuildManifest } from "next/dist/client/route-loader";
 import { useState, useEffect, React } from "react";
+import { CheckIcon } from "@heroicons/react/outline/esm";
 
 export default function UserProfile() {
     const [orders, setOrders] = useState([]);
-    const [items, setItems] = useState([]);
-    const [state, setState] = useState([]);
-    const [cart, setCart] = useState([]);
-    const [total, setTotal] = useState(0.0);
-    const [totalCarbonSavings, setTotalCarbonSavings] = useState(0.0);
 
     useEffect(() => {
         getOrders();
-        getItems();
-        getCart();
-        fetchItemsHandler();
-        fetchCartItemsHandler();
-        fetchOrderedItemsHandler();
-        getTotalCarbonSavings();
     }, []);
 
     function getOrders() {
@@ -38,118 +28,6 @@ export default function UserProfile() {
             });
     }
 
-    function getCart() {
-        fetch("http://localhost:8080/api/v1/cart", {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: "Bearer " + localStorage.getItem("token"),
-            },
-        })
-            .then((response) => response.json())
-            .then((product) => {
-                setCart(product);
-                console.log(state);
-                localStorage.setItem("myCart", JSON.stringify(state));
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    }
-
-    function getItems() {
-        fetch("http://localhost:8080/api/v1/items", {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: "Bearer " + localStorage.getItem("token"),
-            },
-        })
-            .then((response) => response.json())
-            .then((product) => {
-                setItems(product);
-                localStorage.setItem("items", JSON.stringify(product));
-                console.log(product);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    }
-
-    function fetchItemsHandler() {
-        const items = JSON.parse(localStorage.getItem("items") || "[]");
-        console.log(items);
-        setItems(items);
-    }
-
-    function fetchCartItemsHandler() {
-        const cart = JSON.parse(localStorage.getItem("myCart") || "[]");
-        console.log(cart);
-        setCart(cart);
-    }
-
-    function fetchOrderedItemsHandler() {
-        const orders = JSON.parse(localStorage.getItem("orders") || "[]");
-        console.log(orders);
-        setOrders(orders);
-    }
-
-    // function getTotalCarbonSavings() {
-    //     fetch("http://localhost:8080/api/v1/order", {
-    //       method: "GET",
-    //       headers: {
-    //         "Content-Type": "application/json",
-    //         Authorization: "Bearer " + localStorage.getItem("token"),
-    //       },
-    //     })
-    //       .then((response) => response.json())
-    //       .then((product) => {
-    //         // setCart(product);
-    //         setOrders(product);
-    //         let x = 0;
-    //         console.log(product);
-    //         localStorage.setItem("orders", JSON.stringify(product));
-    //         product.cartItems.map(
-    //           (item) => (x += item.carbontotal)
-    //           // console.log(total)
-    //         );
-    //         //  console.log(x);
-    //         setTotalCarbonSavings(x);
-    //         return total;
-    //       })
-    //       .catch((err) => {
-    //         console.log(err);
-    //       });
-    //   }
-
-    function getCarbonSavings() {
-        let x = 0;
-        {
-            orders.map((order) => (
-                <div>
-                    {order.cartItems.map((item) => (
-                        x += item.carbontotal
-                    ))}
-                </div>
-            ))
-        }
-        return x;
-    }
-
-    function getTotalCarbonSavings() {
-        let x = 0;
-        {
-            orders.map((order) => (
-                <div>
-                    {order.cartItems.map((item) => (
-                        x += item.carbontotal
-                    ))}
-                </div>
-            ))
-        }
-        return x;
-    }
-
     return (
         <div>
             <table className="table-auto min-w-full">
@@ -159,6 +37,8 @@ export default function UserProfile() {
                         <th className="py-3 px-6">Item(s)</th>
                         <th className="py-3 px-6">Carbon savings</th>
                         <th className="py-3 px-6">Subtotal</th>
+                        <th className="py-3 px-6">Collected</th>
+                        <th className="py-3 px-6">Donated</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -173,6 +53,24 @@ export default function UserProfile() {
                             ))}
                             <td className="p-5 text-center">{order.carbonTotal}</td>
                             <td className="p-5 text-center">{order.total}</td>
+                            <td className="p-5 text-center">
+                                {order.collected == 1 ? (
+                                    <button>
+                                        <CheckIcon className="h-5 w-5"></CheckIcon>
+                                    </button>
+                                ) : (
+                                    order.donated == 0 ? (
+                                        "pending collection"
+                                    ) : ("-")
+                                )}
+                            </td>
+                            <td className="p-5 text-center">
+                                {order.donated == 1 ? (
+                                    <button>
+                                        <CheckIcon className="h-5 w-5"></CheckIcon>
+                                    </button>
+                                ) : ("-")}
+                            </td>
                         </tr>
                     ))}
                 </tbody>
